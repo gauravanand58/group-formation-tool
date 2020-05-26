96fdaff3cac54860ac0515722a0079eefc7c5d02
Srikrishna Sasidharan <sr950863@dal.ca>
2020-05-26 16:34:57 -0300

    Revert "check runner and project initial setup"
    This reverts commit 491d9e512e940987f2dde0de495c66e691379472

--- src/main/java/com/advsdc/group4/utils/Utils.java
+++
@@ -1,7 +0,0 @@
-package com.advsdc.group4.utils;
-/*This class hold all common methods which are to be used across the application */
-public class Utils {
-	public static String emptyCheck(String value) {
-		return (value!=null && value!="")? value : "";		
-	}
-}
