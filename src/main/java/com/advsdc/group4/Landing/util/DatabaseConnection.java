3fa06a60b5f0d7c8fc0c75158c4c18971fc58768
Srikrishna Sasidharan <sr950863@dal.ca>
2020-05-27 16:03:44 -0300

    Database-Initial-Setup-For-DEV

---
+++ src/main/java/com/advsdc/group4/Landing/util/DatabaseConnection.java
@@ -0,0 +1,36 @@
+package com.advsdc.group4.Landing.util;
+
+import java.sql.Connection; 
+import java.sql.DriverManager; 
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+  
+@Component
+public class DatabaseConnection {
+	 private static Connection con = null; 
+	  
+ 	@Autowired
+	DBConfiguration configuration;
+ 
+	    public  Connection getConnection() 
+	    { 
+	    	String url = configuration.getUrl(); 
+	        String user = configuration.getUsername(); 
+	        String pass = configuration.getPassword(); 
+	    	
+	        try { 
+	        	System.out.println("check:"+url);
+	            Class.forName(configuration.getDriver()).getClass(); 
+	            con = DriverManager.getConnection(url, user, pass); 
+	        } 
+	        catch (ClassNotFoundException | SQLException e) { 
+
+	            System.out.println("DbConnect:"+e);
+	        	e.printStackTrace();
+       } 
+	        return con; 
+    } 
+
+}
