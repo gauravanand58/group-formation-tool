72b5603b611d455b75f69286095d086615fad908
Srikrishna Sasidharan <sr950863@dal.ca>
2020-05-29 21:44:56 -0300

    environment based db configuration

--- src/main/java/com/advsdc/group4/Group4Application.java
+++ src/main/java/com/advsdc/group4/Group4Application.java
@@ -5,26 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-import com.advsdc.group4.Landing.util.DBConfiguration;
+import com.advsdc.group4.util.DBConfiguration;
 
 
 @SpringBootApplication(exclude = {
 SecurityAutoConfiguration.class })
 
-@EnableConfigurationProperties
-
 public class Group4Application {
 	
 	 
 	public static void main(String[] args) {
-		//SpringApplication.run(Group4Application.class, args);
-		
-		final ApplicationContext ctx = SpringApplication.run(Group4Application.class, args);
-		final DBConfiguration dbConfig = ctx.getBean(DBConfiguration.class);
-		System.out.println(dbConfig.getUrl());
-		System.out.println(dbConfig.getDriver());
-
+		SpringApplication.run(Group4Application.class, args);
+//		final ApplicationContext ctx = SpringApplication.run(Group4Application.class, args);
+//	final DBConfiguration dbConfig = ctx.getBean(DBConfiguration.class);
+//		System.out.println(dbConfig.getUrl());
+//		System.out.println(dbConfig.getDriver());
 	}
 	
 
--- src/main/java/com/advsdc/group4/Landing/service/LandingService.java
+++
@@ -1,8 +0,0 @@
-package com.advsdc.group4.Landing.service;
-
-import org.springframework.ui.Model;
-
-public interface LandingService {
-	
-	public String landingHome(Model modal);
-}
