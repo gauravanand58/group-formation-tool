3d5a671b70e2e371a9ff8962183194f4b3c30b58
Srikrishna Sasidharan <sr950863@dal.ca>
2020-06-01 12:41:43 -0300

    Revert "Merge branch 'feature-user-forgot-password' into 'develop'"
    This reverts merge request !16

--- src/main/java/com/advsdc/group4/Group4Application.java
+++ src/main/java/com/advsdc/group4/Group4Application.java
@@ -3,6 +3,11 @@ package com.advsdc.group4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.advsdc.group4.util.DBConfiguration;
 
 
 @SpringBootApplication(exclude = {
