849d57eb28ba5be3e62837a3cab3f04763c3afeb
Srikrishna Sasidharan <sr950863@dal.ca>
2020-06-17 00:13:20 -0300

    DB COnfig

--- src/main/java/CSCI5308/GroupFormationTool/Database/DefaultDatabaseConfiguration.java
+++ src/main/java/CSCI5308/GroupFormationTool/Database/DefaultDatabaseConfiguration.java
@@ -2,13 +2,10 @@ package CSCI5308.GroupFormationTool.Database;
 
 public class DefaultDatabaseConfiguration implements IDatabaseConfiguration
 {
-//	private static final String URL = System.getenv("URL");
-//	private static final String USER = System.getenv("USER");
-//	private static final String PASSWORD = System.getenv("PASSWORD");
+	private static final String URL = System.getenv("URL");
+	private static final String USER = System.getenv("USER");
+	private static final String PASSWORD = System.getenv("PASSWORD");
 
-	private static final String URL = "jdbc:mysql://db-5308.cs.dal.ca:3306/CSCI5308_4_DEVINT?serverTimezone=AST";
-	private static final String USER = "CSCI5308_4_DEVINT_USER";
-	private static final String PASSWORD = "CSCI5308_4_DEVINT_4202";
 	public String getDatabaseUserName()
 	{
 		return USER;
