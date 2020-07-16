b00802cb1d06af2bbc4a37b42c02d28cad9bb601
Shrey <shrey.r.vaghela@gmail.com>
2020-07-16 14:14:44 -0300

    Fixed bug to handle errors

--- src/main/java/CSCI5308/GroupFormationTool/Survey/SurveyStudentController.java
+++ src/main/java/CSCI5308/GroupFormationTool/Survey/SurveyStudentController.java
@@ -21,7 +21,7 @@ public class SurveyStudentController {
 		ISurvey survey=new Survey();
 		ISurveyStudentPersistence surveyDB = SurveySystemConfig.instance().getStudentSurveyDB();
 		boolean responsesaved = survey.submitResponse(surveyDB, bannerID, courseid, studentResponse);
-		int  isUserInstructor = (responsesaved)?0:1;
+		int  isUserInstructor = (responsesaved)?0:-1;
 		return "redirect:/course/course?id="+courseId+"&isUserInstructor="+isUserInstructor+"&BannerID="+BannerID;		
 	}
 }
